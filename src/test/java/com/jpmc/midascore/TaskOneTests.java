ca41b18444cafc9d1d23e55e53c3697fab2f894c
Sultan O. <154644276+onlysully@users.noreply.github.com>
2025-09-09 22:24:38 -0400

    Update TaskOneTests.java

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 class TaskOneTests {
